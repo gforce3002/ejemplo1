51235e392e5b997268de79d709e3b94ad1708b5c
Francisco Ivan Ramirez Alcaraz <gforce3002@gmail.com>
2021-10-20 20:17:30 -0500

    Agregando un navbar

--- pages/Blog/_id.vue
+++ pages/Blog/_id.vue
@@ -3,8 +3,9 @@
       <h1>Detalles del articulo: {{$route.params.id}}</h1>
       <div class="card">
         <div class="card-body">
-          <h1>{{articulo.title}}</h1>
-          <p>{{articulo.body}}</p>
+          <h1>{{articulo.data.title}}</h1>
+          <small>{{articulo.autor.name}}</small>
+          <p>{{articulo.data.body}}</p>
           <nuxt-link class="btn btn-primary" to="/blog">Regresar</nuxt-link>
         </div>
       </div>
@@ -16,17 +17,36 @@ import axios from 'axios'
 export default {
   data(){
     return{
-      articulo:{}
+      //articulo:{data:{},autor:{}}
     }
   },
   async created(){
-    try {
+    /* try {
       const id = this.$route.params.id
-      const resp = await axios.get("https://jsonplaceholder.typicode.com/posts/"+id)      
-      this.articulo = resp.data
-      console.log(resp)
+      const resp = await axios.get("https://jsonplaceholder.typicode.com/posts/"+id)
+      const respAutor = await axios.get("https://jsonplaceholder.typicode.com/users/"+resp.data.userId)      
+      this.articulo.data = resp.data
+      this.articulo.autor = respAutor.data
+
+      console.log(resp,respAutor)
     } catch (error) {
       console.log(error)
+    } */
+  },
+  async asyncData({isDev, route, store, env, params, query, req, res, redirect, error}) {
+    try {
+      const id = params.id
+      const resp = await axios.get("https://jsonplaceholder.typicode.com/posts/"+id)
+      const respAutor = await axios.get("https://jsonplaceholder.typicode.com/users/"+resp.data.userId)      
+      const articulo = {}
+      articulo.data = resp.data
+      articulo.autor = respAutor.data
+
+      console.log(articulo)
+      return {articulo}
+    } catch (error) {
+      console.log(error)
+      return {error: error}
     }
   }
 }
